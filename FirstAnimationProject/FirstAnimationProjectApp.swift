7dd4df69d35bdadf404a363339299ebe0e5ad945
Judy <jyu@lcs.on.ca>
2022-04-21 11:22:22 -0400

    Initial Commit

---
+++ FirstAnimationProject/FirstAnimationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAnimationProjectApp.swift
+//  FirstAnimationProject
+//
+//  Created by Judy Yu on 2022-04-21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAnimationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
